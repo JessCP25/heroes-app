187f147f8f94e696c8ab0ad10f994679926b4234
JessCP25 <jpayanoc25@gmail.com>
2024-01-21 20:09:51 -0500

    Método para mantener sesión del usuario

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,27 +2,40 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+import { ThisReceiver } from '@angular/compiler';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
-
   private baseUrl = environments.baseUrl;
   private user?: User;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  get currentUser():User|undefined {
-    if(!this.user) return undefined;
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
     return structuredClone(this.user);
   }
 
-  login(email: string, password: string):Observable<User> {
-    return this.http.get<User>(`${this.baseUrl}/users/1`)
-      .pipe(
-        tap(user => this.user = user),
-        tap(user => localStorage.setItem('token', 'df454sv-´{.dfgsdfds.fds456d')),
-      );
+  login(email: string, password: string): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      tap((user) =>
+        localStorage.setItem('token', 'df454sv-{.dfgsdfds.fds456d')
+      )
+    );
+  }
+
+  checkAuthentication(): Observable<boolean> {
+    if (!localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      map((user) => !!user),
+      catchError(() => of(false))
+    );
   }
 
   logout() {
